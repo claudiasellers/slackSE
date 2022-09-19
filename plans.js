39dd935f3e6a576467f0eef8024b2a806d63f1db
claudiasellers <claudiasellers1@gmail.com>
2022-09-19 11:59:33 -0600

    adding plans.js

---
+++ plans.js
@@ -0,0 +1,122 @@
+var planFeatures = [
+	{
+		category: "Channel Features",
+		feature : "Workflow Builder",
+		details : "No code tool to automate common workflows - includes pre built templates.",
+		lowestAvailablePlan : "Pro",
+		isIntegration: false
+	},
+	{
+		category: "Channel Features",
+		feature: "Message Activity",
+		details: "Ability to see engagement stats on messages",
+		lowestAvailablePlan: "Business Plus",
+		isIntegration: false
+	},
+	{
+		category: "Channel Features",
+		feature: "Multi-Workspace Channels",
+		details: "Connect multiple workspaces together.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: false
+	},
+	{
+		category: "Administration at Scale",
+		feature: "Granular Admin Roles (aka System Roles)",
+		details: "Assign admin users but just with specific abilities.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: false
+	},
+	{
+		category: "Administration at Scale",
+		feature: "Centralized Channel Management",
+		details: "Edit all channels from one central Dashboard.",
+		lowestAvailablePlan: "Business Plus",
+		isIntegration: false
+	},
+	{
+		category: "Administration at Scale",
+		feature: "Admin APIs",
+		details: "Automate org/workspace/app mgmt",
+		lowestAvailablePlan: "Enterprise Select",
+		isIntegration: false
+	},
+	{
+		category: "Administration at Scale",
+		feature: "Custom Data Retention",
+		details: "Applies to public/private channels, DMs and files. Lifetime by default.",
+		lowestAvailablePlan: "Pro",
+		isIntegration: false
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "SAML SSO",
+		details: "Pre-built connectors w/ many IDPs.",
+		lowestAvailablePlan: "Business Plus",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Multi-SAML SSO",
+		details: "Multiple SSO Providers connected to the same Grid Org.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "SCIM API",
+		details: "Create/deactivate members, sync & update profile fields, create/delete user groups & sync info from IDP.",
+		lowestAvailablePlan: "Business Plus",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "SCIM API (Guest Provisioning)",
+		details: "Provision and manage Multi-Channel Guest accounts in bulk.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "IDP Groups",
+		details: "Auto-assign groups of users to Workspace",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Session Management",
+		details: "Logout user on a specific device (also available via the Admin API)",
+		lowestAvailablePlan: "Enterprise Select",
+		isIntegration: false
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Domain Claiming",
+		details: "Prevent unsanctioned workspace creation.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: false
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Mobile Device Management (MDM)",
+		details: "MDM Via App Config vendor.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Mobile Application Management (MAM)",
+		details: "App-specific mobile controls. Only available with Microsoft Intune.",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: true
+	},
+	{
+		category: "Identity and Device Management",
+		feature: "Native Slack Mobile Controls (Non EMM)",
+		details: "Block download/copy, block jailbroken/rooted devices, default browser control, secondary auth",
+		lowestAvailablePlan: "Enterprise Grid",
+		isIntegration: false
+	}
+
+]
